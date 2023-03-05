f8a7612f7d1cd0abe0edcc5d00d42b5bd4561f6b
Titaniumfire34 <z-sap@live.com>
2023-03-05 17:02:24 -0500

    aad temp comments

--- kitchen.java
+++ kitchen.java
@@ -1,3 +1,8 @@
 public class kitchen {
-    
+   /*  printorders()
+        completeTicket(ticketID) //order has been made
+        Kitchen variables:
+        Linked list of active orders
+    */
+
 }
--- table.java
+++ table.java
@@ -1,3 +1,19 @@
 public class table {
-    
+    /* 
+     * makeTicket(seatNumber) //call add customer if seat # is new
+        addCustomer()
+        removeCustomer(seatNumber)
+makeTable()
+adjustTable(tableNumber)
+Table Variables
+Int tableNumber
+Int numberOfSeats
+Int ticketID
+Int queuePosition
+int seatNumber
+Hash table of linked list of orders (key is seat number, value orders())
+
+     * 
+     * 
+    */
 }
