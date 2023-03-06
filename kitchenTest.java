a8c24b370d6b308f9f945ed430e134895a6f667f
Titaniumfire34 <z-sap@live.com>
2023-03-06 15:50:51 -0500

    added in test with comments to tell general idea

--- kitchenTest.java
+++ kitchenTest.java
@@ -1,3 +1,69 @@
+
+import java.util.*;
+import java.util.regex.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class kitchenTest {
+    private LinkedList<ticket> orders = null;
+    private LinkedList<ticket> orders2 = null;
+    private LinkedList<ticket> orders3 = null;
+
+    @Before
+    public void setupTest() {
+        orders = new LinkedList<ticket>(); // > 1 order
+        orders2 = new LinkedList<ticket>(); //1 order
+        orders3 = new LinkedList<ticket>(); //empty
+
+    }
+
+    @Test(timeout = 2000)
+    public void printOrderTest1() { //print out orders 1
+
+    }
+
+    @Test
+    public void printOrderTest2() { //printout orders 2
+        
+    }
+
+    @Test
+    public void printOrderTest3() { //printout orders 3
+        
+    }
+
+    @Test
+    public void printOrderTest4() { //printout variation of orders 1
+        
+    }
+
+    @Test
+    public void completeTicketTest1() { //test orders 1 completing all orders
+        
+    }
+
+    @Test
+    public void completeTicketTest2() { //test orders 1 completing some orders
+        
+    }
+
+    @Test
+    public void completeTicketTest3() { //test orders 2 completing all orders
+        
+    }
+
+    @Test
+    public void completeTicketTest4() { //test orders 2 completing no orders
+        
+    }
+
+    @Test
+    public void completeTicketTest5() { //failing/passing orders 3 because it empty
+        
+    }
+
+    public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("kitchenTest");
+	}
     
 }
--- tableTest.java
+++ tableTest.java
@@ -1,3 +1,156 @@
+import java.util.*;
+import java.util.regex.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class tableTest {
+
+    private int tableNumber;
+    private int tableNumber2;
+    private int tableNumber3;
+    private int numberOfSeats;
+    private int numberOfSeats2;
+    private int numberOfSeats3;
+    private HashMap<Integer,ticket> table = null;
+    private HashMap<Integer,ticket> table2 = null;
+    private HashMap<Integer,ticket> table3 = null;
+
+    @Before
+    public void setupTest() { //setup all variations of tables to test each one
+        table = new HashMap<Integer,ticket>();
+        table2 = new HashMap<Integer,ticket>();
+        table3 = new HashMap<Integer,ticket>();
+    }
+
+    @Test
+    public void makeTableTest1() { //test for making table 1 which is normal table
+
+    }
+
+    @Test
+    public void makeTableTest2() { //test for making table 2 which is 1 seat table
+
+    }
+
+    @Test
+    public void makeTableTest3() { //test for making table 3 which is no seat table
+
+    }
+
+    @Test
+    public void adjustTableTest1() { //edit table 1 test
+
+    }
+
+    @Test
+    public void adjustTableTest2() { // edit table 1 test
+
+    }
+
+    @Test
+    public void adjustTableTest3() { // edit table 2 test
+
+    }
+
+    @Test
+    public void adjustTableTest4() { // edit table 2 test
+
+    }
+
+    @Test
+    public void adjustTableTest5() { // edit table 3 test
+
+    }
     
+    @Test
+    public void makeTicketTest1() { //make ticket for table 1
+
+    }
+
+    @Test
+    public void makeTicketTest2() { //make ticket for table 1
+
+    }
+
+    @Test
+    public void makeTicketTest3() { //make ticket for table 2
+
+    }
+
+    @Test
+    public void makeTicketTest4() { //make ticket for table 2
+
+    }
+
+    @Test
+    public void makeTicketTest5() { //fail to make ticket for table 3
+
+    }
+
+    @Test
+    public void removeCustomerTest1() { //pass to remove customer for table 1
+
+    }
+
+    @Test
+    public void removeCustomerTest2() { //failto remove customer for table 1
+
+    }
+
+    @Test
+    public void removeCustomerTest3() { //pass to remove customer for table 2
+
+    }
+
+    @Test
+    public void removeCustomerTest4() { //fail to remove customer for table 2
+
+    }
+
+    @Test
+    public void removeCustomerTest5() { //fail to remove customer for table 3
+
+    }
+
+    @Test
+    public void addCustomerTest1() { //pass to add customer for table 1
+
+    }
+
+    @Test
+    public void addCustomerTest2() { //pass to add customer for table 1
+
+    }
+
+    @Test
+    public void addCustomerTest3() { //pass to add customer for table 2
+
+    }
+
+    @Test
+    public void addCustomerTest4() { //failto remove customer for table 1 and 2
+
+    }
+
+    @Test
+    public void addCustomerTest5() { //pass and fail to add customer for table 3
+
+    }
+
+    @Test
+    public void getTicketTest1() {   //get ticket table 1 and then fail to get ticket out of range 
+    }
+
+    @Test
+    public void getTicketTest2() {   //get ticket table 2 and then fail to get ticket out of range  
+    }
+
+    @Test
+    public void getTicketTest3() {    //fail to get ticket from table 3 
+    }
+    
+    public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("tableTest");
+	}
+
 }
