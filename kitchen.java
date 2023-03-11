cd7f13566707a9016bb265eae8b3c1c76505909b
sjok <sjok101@gmail.com>
2023-03-11 13:08:11 -0500

    adding table and kitchen

--- kitchen.java
+++ kitchen.java
@@ -1,3 +1,25 @@
+import java.util.LinkedList;
+
 public class kitchen {
-    
+
+    public LinkedList<ticket> tickets = null;
+
+
+    public kitchen() { //Constructor?
+        this.tickets = new LinkedList<ticket>();
+    }
+
+
+    public String printOrders() {
+        String printedOrders = "";
+        return printedOrders;
+    }
+
+    public boolean completeTicket(ticket t) {
+        //this.tickets.get(t);
+        Boolean completed = false;
+        return completed;
+    }
+
+
 }
--- ui.java
+++ ui.java
@@ -1,25 +1,27 @@
 public class ui {
     public static void main(String[] args){
         //this is an login example, users are added as samples upon scan, type "test" as user and "fail" as password to example a mismatched case
-        while(!userLogin.initLogin()){
-            System.out.println("Please login.");
-        }
-
+        login();
+        pos();
         
+
     }
 
 
     public static void pos(){
-
+        while(true){
+            mainMenu();
+        }
     }
 
     public static void mainMenu(){
 
     }
 
-    public static boolean login(){
-
-        return true;
+    public static void login(){
+        while(!userLogin.initLogin()){
+            System.out.println("Please login.");
+        }
     }
 
     public static void shutDown(){
