103997aa29cc4adb831e3ebe2bc7c2f1acadec41
ewalter5 <ewalter2055@gmail.com>
2023-04-05 18:28:28 -0400

    added button

--- ReactFiles/pos-react/src/App.js
+++ ReactFiles/pos-react/src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import styled, { createGlobalStyle, keyframes, css } from "styled-components";
 
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`
-;
+// const Button = styled.button`
+//   background-color: black;
+//   color: white;
+//   font-size: 20px;
+//   padding: 10px 60px;
+//   border-radius: 5px;
+//   margin: 10px 0px;
+//   cursor: pointer;
+// `
+// ;
 // Usage
 
 
 function sayHello() {
   alert('You clicked me!');
 }
+
+{/* <Button onClick={sayHello}>
+Disabled Button
+</Button> */}
 
 function App() {
   return (
@@ -26,12 +30,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           hey guys.
-          <Button onClick={sayHello}>
-            Disabled Button
-          </Button>
+          
         </p>
         
       </header>
+      <button type="button" onClick={sayHello}>Click Me</button>
     </div>
   );
 }
