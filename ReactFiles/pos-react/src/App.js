bb00877f006c7eb9a8bbedfa79b5a70da836098b
ewalter5 <ewalter2055@gmail.com>
2023-04-06 01:00:26 -0400

    created component

--- ReactFiles/pos-react/src/App.js
+++ ReactFiles/pos-react/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import HeaderBar from './HeaderBar';
 
-// const Button = styled.button`
-//   background-color: black;
-//   color: white;
-//   font-size: 20px;
-//   padding: 10px 60px;
-//   border-radius: 5px;
-//   margin: 10px 0px;
-//   cursor: pointer;
-// `
-// ;
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`
+;
 // Usage
 
 
@@ -19,21 +19,21 @@ function sayHello() {
   alert('You clicked me!');
 }
 
-{/* <Button onClick={sayHello}>
+ <Button onClick={sayHello}>
 Disabled Button
-</Button> */}
+</Button> 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <HeaderBar />
+      <div className='header component'>
+        <h1>Ticket Screen</h1>
+      </div>
         <p>
-          hey guys.
           
         </p>
         
-      </header>
       <button type="button" onClick={sayHello}>Click Me</button>
     </div>
   );
---
+++ ReactFiles/pos-react/src/HeaderBar.js
@@ -0,0 +1,15 @@
+const HeaderBar = () => {
+    return (  
+        <nav className="navbar">
+            <h1> Ticket screen header</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/takeout">TakeOut</a>
+
+            </div>
+
+        </nav>
+    );
+}
+ 
+export default HeaderBar;
