f8757a466d43e4f5e96cbf807a7b67b9dbc36fc5
Titaniumfire34 <z-sap@live.com>
2023-04-06 19:17:24 -0400

    add react

---
+++ react_files/pos-react/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1> Table </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
