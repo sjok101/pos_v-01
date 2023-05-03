a108927cec0e2503d1cdd93ef6134a10c587c248
ewalter5 <ewalter2055@gmail.com>
2023-05-03 19:13:53 -0400

    added springboot package

---
+++ tickets/src/test/java/dev/pos/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pos.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
