e9982346d765d6e27587eac09c808e94ea4fb0d6
Titaniumfire34 <z-sap@live.com>
2023-05-06 15:19:45 -0400

    added ticket deletion in kitchen

--- uiControl/KitchenSceneController.java
+++ uiControl/KitchenSceneController.java
@@ -116,4 +116,52 @@ public class KitchenSceneController implements Initializable{
             e.printStackTrace();
         }
     }
+
+    @FXML
+    private void deletedKitchenTicket(ActionEvent event) {
+        kitchenTicket k = kitchenview.getSelectionModel().getSelectedItem();
+        kitchenview.getItems().removeAll(kitchenview.getSelectionModel().getSelectedItem());
+        kitchenview.refresh();
+        int kTable = 0;
+        int tickID = 0;
+        for(kitchenTicket ta: currTickets) {
+            if(ta.equals(k) == true) {
+                currTickets.remove(k);
+                kTable = ta.getTableNum();
+                tickID = ta.getTicketid();
+                try {
+                    kj.KitchenTicketsToJson(currTickets);
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+        ticketJson tj = new ticketJson();
+        try {
+            LinkedList<Ticket> tickets = tj.getTicketsJson();
+            for(Ticket t: tickets) {
+                if(t.getTicketID() == tickID && t.getTableNum() == kTable) {
+                    tickets.remove(t);
+                    tj.ticketsToJson(tickets);
+                    break;
+                }
+            }
+            tableJson tja = new tableJson();
+            LinkedList<table> tables = tja.getTablesJson();
+            for(table t: tables) {
+                if(t.getTicket() == tickID && t.getTableNumber() == kTable) {
+                    tables.remove(t);
+                    t.setTicket(0);
+                    tables.add(t);
+                    tja.tablesToJson(tables);
+                    break;
+                }
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
